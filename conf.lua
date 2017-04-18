ba0b59c6f1a830e6bd0d096a538884ceb9fc80c3
fuzzwizard <mitch@fyreapp.com>
2017-04-18 09:27:45 -0700

    initial commit

---
+++ conf.lua
@@ -0,0 +1,3 @@
+function love.conf(f)
+    -- Configure stuff!
+end
---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    -- Loadtime callback
+end
+
+function love.update()
+    -- Update loop callback
+end
+
+function love.draw()
+    -- Render loop callback
+end
+
+function love.quit()
+    -- Quittime callback
+end
