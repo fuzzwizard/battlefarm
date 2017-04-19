a8dae1b457bd07e51da998fcbfbcbd4399652516
fuzzwizard <mitch@fyreapp.com>
2017-04-19 08:49:55 -0700

    early implementation attempts

--- main.lua
+++ main.lua
@@ -1,9 +1,25 @@
+local tiny = require "lib.tiny"
+
+local World = tiny.world()
+
+local ControlSystem = tiny.processingSystem()
+
+ControlSystem.filter = tiny.requireAll(
+    "point",
+    "velocity",
+    "playerControlable"
+)
+
+function ControlSystem:process(e, dt)
+
+end
+
 function love.load()
     -- Loadtime callback
 end
 
-function love.update()
-    -- Update loop callback
+function love.update(dt)
+    World:update(dt)
 end
 
 function love.draw()
