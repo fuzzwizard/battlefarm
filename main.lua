0759b6a1aad24e9d8ac555a250811e07bc3822f1
fuzzwizard <mitch@fyreapp.com>
2017-04-21 08:46:12 -0700

    this is broken

--- main.lua
+++ main.lua
@@ -1,24 +1,36 @@
-local tiny = require "lib.tiny"
-
-local World = tiny.world()
+local v2    = require "lib.hump.vector"
+local tiny  = require "lib.tiny"
 
 local ControlSystem = tiny.processingSystem()
 
-ControlSystem.filter = tiny.requireAll(
-    "point",
-    "velocity",
-    "playerControlable"
-)
+ControlSystem.filter = tiny.requireAll("velocity", "point")
 
 function ControlSystem:process(e, dt)
-
+    e:move(v(1, 1))
 end
+
+local World = tiny.world(ControlSystem)
+
+local Player = {}
+
+function Player:new(point, velocity)
+    self.point = point
+    self.velocity = velocity
+end
+
+function Player:move(v)
+    self.point = self.point + v
+end
+
+World:add(Player)
 
 function love.load()
     -- Loadtime callback
 end
 
 function love.update(dt)
+    -- livereload
+    love.filesystem.load("main.lua")()
     World:update(dt)
 end
 
