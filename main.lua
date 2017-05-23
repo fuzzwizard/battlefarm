b08aca17ad9ff9df5f4636e48cb06878897d73f0
fuzzwizard <mitch@fyreapp.com>
2017-05-23 11:09:58 -0700

    some changes i guess

--- main.lua
+++ main.lua
@@ -3,26 +3,11 @@ local tiny  = require "lib.tiny"
 
 local ControlSystem = tiny.processingSystem()
 
-ControlSystem.filter = tiny.requireAll("velocity", "point")
+ControlSystem.filter = tiny.requireAll("velocity", "point", "move")
 
 function ControlSystem:process(e, dt)
     e:move(v(1, 1))
 end
-
-local World = tiny.world(ControlSystem)
-
-local Player = {}
-
-function Player:new(point, velocity)
-    self.point = point
-    self.velocity = velocity
-end
-
-function Player:move(v)
-    self.point = self.point + v
-end
-
-World:add(Player)
 
 function love.load()
     -- Loadtime callback
@@ -31,7 +16,6 @@ end
 function love.update(dt)
     -- livereload
     love.filesystem.load("main.lua")()
-    World:update(dt)
 end
 
 function love.draw()
